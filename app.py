3425e5932c357178b1c832977f7d76fc92c077b1
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2022-04-23 18:23:40 +0530

    initial Flask

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="AbCd@123", database="railway_system")
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html");
+
+
+@app.route('/admin_login')
+def admin_log():
+    return render_template("admin_log.html");
+
+
+@app.route('/user_login')
+def user_log():
+    return render_template("user_login.html");
+
+
+@app.route('/Registration_screen')
+def user_register():
+    return render_template("Registration_screen.html");
+
+
+if __name__ == '__main__':
+    app.run()

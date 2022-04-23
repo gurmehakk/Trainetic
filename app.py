05fb162a36fc7695f9973568983a249e8c4c1b70
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2022-04-23 20:36:50 +0530

    Contact us screen done lmao xd

--- app.py
+++ app.py
@@ -31,5 +31,10 @@ def aboutuspage():
     return render_template("about_us.html");
 
 
+@app.route('/Contact_us')
+def contact_us():
+    return render_template("contact_us.html")
+
+
 if __name__ == '__main__':
     app.run()

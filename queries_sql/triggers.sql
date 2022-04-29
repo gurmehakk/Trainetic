498561548252ee0b989246f18eea45936f68a815
Krishnam Omar <84073881+krishnamomar@users.noreply.github.com>
2022-04-30 01:26:55 +0530

    triggers done

--- queries_sql/triggers.sql
+++ queries_sql/triggers.sql
@@ -7,9 +7,9 @@ CREATE TRIGGER check_pass
     ON users FOR EACH ROW
 BEGIN
     if(length(new.passwords)<7) then
-		signal sqlstate '45000' set message_text = "Password length cannot be less than 7 characters"; 
+		signal sqlstate '45000' set message_text = "Password length cannot be less than 7 characters";
 	end if;
-END$$    
+END$$
 DELIMITER ;
 
 drop trigger if exists after_ticket_canceled;
@@ -29,10 +29,10 @@ BEGIN
     set @trainid = old.Train_id;
     set @check1 =(SELECT p.Start_terminal_id FROM Passenger p WHERE p.Train_id = @trainid);
     set @check2 =(SELECT p.End_terminal_id FROM Passenger p WHERE p.Train_id = @trainid);
-	
+
     UPDATE Passenger set Start_terminal_id= @startterminal WHERE @check1= @prevstartterminal AND Train_id = @trainid;
     UPDATE Passenger set End_terminal_id= @endterminal WHERE @check2= @prevendterminal AND Train_id = @trainid;
-END$$    
+END$$
 
 DELIMITER ;
 
@@ -42,7 +42,7 @@ CREATE TRIGGER after_ticket_canceled
 BEFORE DELETE
 ON passenger FOR EACH ROW
 BEGIN
-	UPDATE Train 
+	UPDATE Train
     set Available_seats = Available_seats + 1
     where Train.Train_id = old.Train_id;
 END$$
@@ -54,7 +54,7 @@ CREATE TRIGGER after_ticket_booked
 BEFORE DELETE
 ON passenger FOR EACH ROW
 BEGIN
-	UPDATE Train 
+	UPDATE Train
     set Available_seats = Available_seats - 1
     where Train.Train_id = old.Train_id;
 END$$
